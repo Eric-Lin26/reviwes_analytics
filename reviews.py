89d9cd6e4e02b20ed683e9ab095bef055a0a6276
Eric Lin <lemon198926@gmail.com>
2020-03-30 11:46:31 +0800

    first commit

---
+++ reviews.py
@@ -0,0 +1,30 @@
+# 留言分析
+
+data = []
+count = 0
+
+x = 0
+y = 0
+with open ("reviews.txt", "r") as f:
+    for line in f:
+        count += 1
+        data.append(line)
+        if count % 10000 == 0:
+            print(count)
+
+print("檔案讀取完了，總共有", count, "筆資料。")
+
+sum_len = 0
+for avg in data:
+    sum_len += len(avg)
+    
+print("留言平均長度為：", sum_len / len(data))
+
+        
+
+# a = 'dsfjdsflksdj'
+# b = "dlikfljdslkfjsdlk"
+
+# avg = len(a) + len(b) / 2
+
+# print(avg)
